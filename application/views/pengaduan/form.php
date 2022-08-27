a653637e6468c30ed89e5e4379d9c2add9a73835
muhammad_alif_d <muhammad_alif_d@students.itb.ac.id>
2022-08-27 11:02:45 +0700

    form pengaduan responsive

--- application/views/pengaduan/form.php
+++ application/views/pengaduan/form.php
@@ -9,51 +9,51 @@
 					<div class="row">
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 									<label for="inlineinput" class="col-form-label" style="text-align:left !important; display: block;">
 										Kolom Konten / Aduan	
 									</label>
 									<em class="text-danger text-small">*Keterangan aduan perihal permasalahan</em>
 								</div>
-								<div class="col-6">
+								<div class="col-12 col-lg-6">
 									<input type="text" required class="form-control input-full" id="aduan-perihal" placeholder="Perihal Pengaduan">
 								</div>
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 									<label for="inlineinput" class="col-form-label" style="text-align:left !important; display: block;">
 										Tanggal Pengaduan	
 									</label>
 									<em class="text-danger text-small">*Tanggal dilakukannya pengaduan</em>
 								</div>
-								<div class="col-4">
+								<div class="col-12 col-lg-4">
 									<input type="date" id="aduan-tanggal" required class="form-control input-full" id="inlineinput" placeholder="Tanggal Pengaduan">
 								</div>
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 									<label for="inlineinput" class="col-form-label" style="text-align:left !important; display: block;">
 										PIC	
 									</label>
 									<em class="text-danger text-small">*Penanggung jawab terkait permasalahan</em>
 								</div>
-								<div class="col-6">
+								<div class="col-12 col-lg-6">
 									<input type="text" required id="aduan-pic" class="form-control">
 								</div>
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 									<label for="inlineinput" class="col-form-label" style="text-align:left !important; display: block;">
 										Pemohon	
 									</label>
 								</div>
-								<div class="col-6">
+								<div class="col-12 col-lg-6">
 									<select required id="aduan-pemohon" class="form-control">
 										<option value="<?= INDV ?>"><?= INDV ?></option>
 										<option value="<?= HUKUM ?>"><?= HUKUM ?></option>
@@ -65,13 +65,13 @@
 					
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 									<label for="inlineinput" class="col-form-label" style="text-align:left !important; display: block;">
 										Media	
 									</label>
 									<em class="text-danger text-small">*Media yang digunakan untuk pengaduan</em>
 								</div>
-								<div class="col-6">
+								<div class="col-12 col-lg-6">
 									<div class="selectgroup selectgroup-secondary selectgroup-pills d-flex flex-column">
 										<div class='radio-group d-flex mb-2'>
 											<?php
@@ -89,16 +89,16 @@
 											}
 											?>
 										</div>
-										<input type="text" placeholder="link profile" required id="sosmed-link" class="form-control">
+										<input type="text" placeholder="link profile" required id="sosmed-link" class="form-control mt-1">
 									</div>
 								</div>
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 								</div>
-								<div class="col-6">
+								<div class="col-12 col-lg-6">
 									<select class="form-control" id="aduan-fitur">
 										<option value="<?= KOMENTAR ?>"><?= KOMENTAR ?></option>
 										<option value="<?= DM ?>"><?= DM ?></option>
@@ -109,21 +109,21 @@
 						</div>
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 									Gambar<br>
 									<em class="text-danger text-small">*Rekomendasi ukuran : <b>640px X 426px</b></em>
 								</div>
-								<div class="col-6">
+								<div class="col-12 col-lg-6">
 									<input type="file" accept="image/png, image/jpeg, , image/jpg" required id="aduan-gambar" class="form-control">
 								</div>
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="form-group form-inline">
-								<div class="col-3">
+								<div class="col-12 col-lg-3">
 									Keterangan
 								</div>
-								<div class="col-6">
+								<div class="col-12 col-lg-6">
 									<textarea id="aduan-keterangan" placeholder="Keterangan lain" class="form-control" rows="6" style="width: 100%;"></textarea>
 								</div>
 							</div>
@@ -132,7 +132,8 @@
 				</div>
 				<div class="card-footer">
 					<button type="submit" class="btn btn-success">Simpan</button>
-					<button type="reset" class="btn btn-default">Bersihkan</button>
+					<button type="reset" class="btn btn-warning ml-2">Bersihkan</button>
+					<button onclick="history.back()" class="btn btn-default ml-2">Kembali</button>
 				</div>
 			</form>
 		</div>
